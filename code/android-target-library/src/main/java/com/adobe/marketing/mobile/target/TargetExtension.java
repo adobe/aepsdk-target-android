9c21d09d139f4cc1cc1689f59a07d1aceefa2033
Swarna Saraf <swsaraf@adobe.com>
2022-11-01 15:24:28 -0700

    Updated project structure for Target extension

---
+++ code/android-target-library/src/main/java/com/adobe/marketing/mobile/target/TargetExtension.java
@@ -0,0 +1,69 @@
+/*
+ Copyright 2022 Adobe. All rights reserved.
+ This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License. You may obtain a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software distributed under
+ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ OF ANY KIND, either express or implied. See the License for the specific language
+ governing permissions and limitations under the License.
+ */
+
+package com.adobe.marketing.mobile.target;
+
+import com.adobe.marketing.mobile.Extension;
+import com.adobe.marketing.mobile.ExtensionApi;
+
+import static com.adobe.marketing.mobile.target.TargetConstants.EXTENSION_NAME;
+import static com.adobe.marketing.mobile.target.TargetConstants.FRIENDLY_NAME;
+import static com.adobe.marketing.mobile.target.TargetConstants.EXTENSION_VERSION;
+import static com.adobe.marketing.mobile.target.TargetConstants.LOG_TAG;
+
+public class TargetExtension extends Extension {
+    /**
+     * Constructor for {@code TargetExtension}.
+     * <p>
+     * It is invoked during the extension registration to retrieve the extension's details such as name and version.
+     *
+     * @param extensionApi {@link ExtensionApi} instance.
+     */
+    protected TargetExtension(final ExtensionApi extensionApi) {
+        super(extensionApi);
+    }
+
+    /**
+     * Retrieve the extension name.
+     *
+     * @return {@link String} containing the unique name for this extension.
+     */
+    @Override
+    protected String getName() {
+        return EXTENSION_NAME;
+    }
+
+    /**
+     * Retrieve the extension friendly name.
+     *
+     * @return {@link String} containing the friendly name for this extension.
+     */
+    @Override
+    protected String getFriendlyName() { return FRIENDLY_NAME; }
+
+    /**
+     * Retrieve the extension version.
+     *
+     * @return {@link String} containing the current installed version of this extension.
+     */
+    @Override
+    protected String getVersion() {
+        return EXTENSION_VERSION;
+    }
+
+    @Override
+    protected void onRegistered() {
+        // getApi().registerEventListener(TargetConstants.EventType.TARGET, TargetConstants.EventSource.REQUEST_CONTENT,
+        //         this::handleTargetRequestEvent
+        // );
+    }
+}
---
+++ code/android-target-library/src/test/java/com/adobe/marketing/mobile/target/TargetTests.java
@@ -0,0 +1,47 @@
+/*
+ Copyright 2022 Adobe. All rights reserved.
+ This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License. You may obtain a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software distributed under
+ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ OF ANY KIND, either express or implied. See the License for the specific language
+ governing permissions and limitations under the License.
+ */
+
+package com.adobe.marketing.mobile.target;
+
+import com.adobe.marketing.mobile.MobileCore;
+import com.adobe.marketing.mobile.Target;
+
+import org.junit.After;
+import org.junit.runner.RunWith;
+import org.junit.Before;
+import org.junit.Test;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({MobileCore.class})
+@SuppressWarnings("unchecked")
+public class TargetTests {
+    @Before
+    public void setup() {
+        PowerMockito.mockStatic(MobileCore.class);
+    }
+
+    @After
+    public void teardown() {
+    }
+
+    @Test
+    public void test_extensionVersion() {
+        // test
+        final String extensionVersion = Target.extensionVersion();
+        assertEquals("extensionVersion API should return the correct version string.", "2.0.0",
+                extensionVersion);
+    }
+}
