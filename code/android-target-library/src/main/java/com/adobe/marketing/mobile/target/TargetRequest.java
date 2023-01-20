614b1483233928f437d5afa611c3cffe6e901031
pravin <pravinpk0707@gmail.com>
2023-01-19 16:11:43 -0800

    Set right the java doc

--- code/android-target-library/src/main/java/com/adobe/marketing/mobile/target/TargetRequest.java
+++ code/android-target-library/src/main/java/com/adobe/marketing/mobile/target/TargetRequest.java
@@ -106,7 +106,7 @@ public class TargetRequest {
     /**
      * Get {@link #contentCallback} for this request
      *
-     * @return {@link AdobeCallback} containing this {@code contentCallback}
+     * @return {@code AdobeCallback<String>} containing this {@code contentCallback}
      */
     public AdobeCallback<String> getContentCallback() {
         return this.contentCallback;
