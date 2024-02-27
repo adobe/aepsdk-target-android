78a58e4f34aa7bb83d2878a56e7c4e342d830a0c
Pravin Prakash Kumar <pravinpk0707@gmail.com>
2024-02-27 10:54:49 -0800

    Update the version constant location path

--- scripts/version.sh
+++ scripts/version.sh
@@ -9,7 +9,7 @@ LINE="==========================================================================
 VERSION_REGEX="[0-9]+\.[0-9]+\.[0-9]+"
 
 GRADLE_PROPERTIES_FILE=$ROOT_DIR"/code/gradle.properties"
-CONSTANTS_FILE=$ROOT_DIR"/code/target/src/main/java/com/adobe/marketing/mobile/Target.java"
+CONSTANTS_FILE=$ROOT_DIR"/code/target/src/phone/java/com/adobe/marketing/mobile/Target.java"
 EXTENSION_VERSION_REGEX="^.*String EXTENSION_VERSION *= *"
 
 help()
